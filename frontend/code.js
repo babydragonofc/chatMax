13d46b13d4c4860e3adbf548b4910e30fe2da68e
babydragonofc <162851040+babydragonofc@users.noreply.github.com>
2024-11-20 11:51:29 -0300

    Update code.js

--- frontend/code.js
+++ frontend/code.js
@@ -95,7 +95,7 @@ const chatNewBtn = document.querySelector("chatName__button")
         chat.style.display = "flex";
 
 
-        websocket = new WebSocket("wss://chatmax-uskl.onrender.com") //wss://chatmax-uskl.onrender.com
+        websocket = new WebSocket("https://chatmax-backend.onrender.com") //wss://chatmax-uskl.onrender.com
         websocket.onmessage = processMessage;
 
         //websocket.onopen = () => {websocket.send(`Usuário: ${user.name} entrou no chat!`)}
